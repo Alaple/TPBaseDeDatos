1c9e418bf65510bacadce20fb2c53041e3d2bf71
Alaple <lucas.palacios@southworks.com>
2020-10-31 13:04:40 -0300

    feat: Add update and delete linq operations

--- TPBaseDeDatos/Program.cs
+++ TPBaseDeDatos/Program.cs
@@ -10,13 +10,16 @@ namespace TPBaseDeDatos
     {
         static void Main(string[] args)
         {
-           AgregarEmpresa("THE BIG BANG THEORY");
-           AgregarEmpresa("FRIENDS");
+            //AgregarEmpresa("THE BIG BANG THEORY");
+            //AgregarEmpresa("FRIENDS");
 
-            ConsultarEmpresa(1);
+            //ConsultarEmpresa(4);
+            //ListarEmpresas();
+            ActualizarEliminar(7, "THE OFFICE", 8);
         }
 
-        static void AgregarEmpresa(string nombre) {
+        static void AgregarEmpresa(string nombre)
+        {
             using (var context = new DBContext())
             {
                 var emp = new Empresa()
@@ -29,12 +32,53 @@ namespace TPBaseDeDatos
             Console.WriteLine("Estudiante agregado!");
         }
 
-        static void ConsultarEmpresa(int id) {
+        static void ConsultarEmpresa(int id)
+        {
             using (var context = new DBContext())
             {
                 var consulta = context.Empresas.Where(s => s.EmpresaId == id);
                 var empresa = consulta.FirstOrDefault<Empresa>();
-                Console.WriteLine("Encontré al estudiante 2 y se llama " + empresa.nombre);
+                Console.WriteLine("Encontré al estudiante " + id + " y se llama " + empresa.nombre);
+            }
+        }
+
+        static void ListarEmpresas()
+        {
+            using (var context = new DBContext())
+            {
+                var empresas = from s in context.Empresas
+                               orderby s.nombre
+                               select s;
+                foreach (var item in empresas)
+                {
+                    Console.WriteLine("Empresas ", item.nombre);
+                }
+            }
+        }
+
+        static void ActualizarEliminar(int idActualizar, string nombreActualizado, int idEliminar)
+        {
+            using (var context = new DBContext())
+            {
+                var listaEstudiantes = context.Empresas.ToList<Empresa>();
+                Console.WriteLine(listaEstudiantes);
+
+                //Agrega una nueva empresa
+                context.Empresas.Add(new Empresa()
+                {
+                    nombre = "THE OFFICE",
+                });
+
+                //Actualiza un estudiante
+                Empresa estudianteActualizado = listaEstudiantes.Where(s =>
+                s.EmpresaId == idActualizar).FirstOrDefault<Empresa>();
+                estudianteActualizado.nombre = nombreActualizado;
+
+                //Elimina un estudiante en posición 3RODRIGO
+                context.Empresas.Remove(listaEstudiantes.ElementAt<Empresa>(idEliminar));
+
+                //Ejecuta instrucciones Insert, Update & Delete en la base de datos
+                context.SaveChanges();
             }
         }
     }

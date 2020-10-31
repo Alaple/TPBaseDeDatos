5ddac1b75039a146e7cfc3965a0cb48e0bb0ca4f
Alaple <lucas.palacios@southworks.com>
2020-10-31 11:53:42 -0300

    Add project files.

---
+++ TPBaseDeDatos/DBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TPClass;
+
+namespace TPBaseDeDatos
+{
+    class DBContext:DbContext
+    {
+        public DbSet<Socio> Socios { get; set; }
+        public DbSet<Empresa> Empresas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SchoolDBCF;Trusted_Connection=True;");
+        }
+    }
+}
